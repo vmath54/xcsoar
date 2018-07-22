6c7ba3ff1783e4976f4125e3c2aabcdb1afc38a0
vmath54 <vmath54@users.noreply.github.com>
2018-07-22 14:19:41 +0200

    getVACfiles.pl. changements du site SIA

--- waypoints/getVACfiles.pl
+++ waypoints/getVACfiles.pl
@@ -2,21 +2,17 @@
 #
 # recuperation des cartes VAC de france, depuis le site SIA
 #
-# sur la page d'accueil (https://www.sia.aviation-civile.gouv.fr), on récupere un lien comme celui-ci, qui correspond au menu "Atlas VAC FRANCE" :
-# <a href="https://www.sia.aviation-civile.gouv.fr/documents/htmlshow?f=dvd/eAIP_27_APR_2017/Atlas-VAC/home.htm" title="">Atlas VAC FRANCE</a>
+# sur la page d'accueil ( https://www.sia.aviation-civile.gouv.fr ), on récupere un lien comme celui-ci, qui correspond au menu "Atlas VAC FRANCE" :
+# <a href="https://www.sia.aviation-civile.gouv.fr/documents/htmlshow?f=dvd/eAIP_19_JUL_2018/Atlas-VAC/home.htm" title="">Atlas VAC FRANCE</a>
 #
-# On récupère la date eAIP ; ici, eAIP_27_APR_2017
-# Ceci permet de construire l'URL de la page qui répertorie toutes les cartes VAC ; dans notre exemple :
-# "https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_27_APR_2017/Atlas-VAC/FR/VACProduitPartie.htm";
+# On récupère la date eAIP ; ici, eAIP_19_JUL_2018
 #
-# grace a cette page, on reconstruit l'URL d'acces aux PDF des cartes VAC ; par exemple :
-# https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_27_APR_2017/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.LFEZ.pdf
+# Ceci permet de récupérer le script JS qui répertorie toutes les cartes VAC ; dans notre exemple :
+# https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_19_JUL_2018/Atlas-VAC/Javascript/AeroArraysVac.js
 #
-# a noter qu'auparavant, l'URL d'acces a une carte etait :
-# https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_27_APR_2017/Atlas-VAC/PDF_AIPparSSection/VAC/AD/2/1706_AD-2.LFEZ.pdf
+# grace a ces infos, on reconstruit l'URL d'acces aux PDF des cartes VAC ; par exemple :
+# https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_19_JUL_2018/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.LFEZ.pdf
 #
-# A noter qu'au 11/01/2017, le certification du site sia n'est pas valide ; d'ou l'option "SSL_NOP_VERIFY" lors des diffents acces
-
 
 use VAC;
 #use LWP::Simple;
@@ -28,41 +24,30 @@ use strict;
 my $dirDownload = "vac";    # le répertoire qui va contenir les documents pdf charges
 my $siteURL = "https://www.sia.aviation-civile.gouv.fr";
 my $baseURL = "https://www.sia.aviation-civile.gouv.fr/dvd/__EAIP__/Atlas-VAC";
-my $pageURL = "$baseURL/FR/VACProduitPartie.htm";  # url de la page de téléchargement des cartes VAC
+#my $pageURL = "$baseURL/FR/VACProduitPartie.htm";  # url de la page de téléchargement des cartes VAC
+my $jsURL = "$baseURL/Javascript/AeroArraysVac.js";  # le code javascript qui contient la liste des terrains
 
+my $infos = {};    # va contenir les infos necessaires a la construction de l'url de chaque doc pdf
 
 {
-  #    --- chargement page d'accueil, pour recuperer eAIP -----
-  print "## recuperation et traitement de la page d'accueil ##\n";
-  print "   $siteURL\n";
-  my ($code, $page, $cookies) = &sendHttpRequest($siteURL, SSL_NO_VERIFY => 1);
-  die "Impossible de charger la page $siteURL" unless (defined($page));
 
-  my $eAIP;
-  if ( $page =~ /SUP AIP.*\?f=dvd\/(eAIP.*?)\/Atlas\-VAC\/home\.htm\"/)    # "SUP AIP" est la rubrique juste avant  "Atlas VAC FRANCE"
-  {
-    $eAIP = $1
-  }
-  else
-  {
-    die "Pas trouvé eAIP dans la page d'accueil";
-  }
+  my $eAIP = &getEAIP($siteURL);    # recuperation de la date eAIP, de la forme "eAIP_19_JUL_2018"
+  
   print "   eAIP = $eAIP\n";
   die "eAIP ne semble pas conforme" if (length($eAIP) > 20);
 
   $baseURL =~ s/__EAIP__/$eAIP/;
-  $pageURL =~ s/__EAIP__/$eAIP/;
-
-  print "\n## recuperation et traitement de la page des cartes VAC ##\n";
-  print "   $pageURL\n";
-  my ($code, $page, $cookies) = &sendHttpRequest($pageURL, SSL_NO_VERIFY => 1);
-  die "Impossible de charger la page $pageURL" unless (defined($page));
+  $jsURL =~ s/__EAIP__/$eAIP/;
+  
+  print "\n## recuperation des codes OACI des terrains repertories ##\n";
+  print "   $jsURL\n";
+  my ($code, $page, $cookies) = &sendHttpRequest($jsURL, SSL_NO_VERIFY => 1);
+  die "Impossible de charger la page $jsURL" unless (defined($page));
   &writeBinFile("page.html", $page);
   #my $page; { local(*INPUT, $/); open (INPUT, "page.html") || die "can't open page.html"; $page = <INPUT>; close INPUT };  # debug, pour lire un fichier html local
 
-  my $infos = &decodePage($page);
-  # print Dumper($infos); exit;
-  my $ads = $$infos{ad};
+  my $ads = &getOACIinfos($jsURL);
+#  print Dumper($ads); exit;
   
   mkdir $dirDownload;
 
@@ -73,8 +58,9 @@ my $pageURL = "$baseURL/FR/VACProduitPartie.htm";  # url de la page de télécharg
     #next if ($ad le "LFPB");    # permet de reprendre l'operation sans recommencer au debut
 	#next if ($ad eq "LFPC");    # permet de ne pas traiter une carte specifique
     my $refAD = $$ads{$ad};
-	# https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_27_APR_2017/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.LFEZ.pdf
-	my $urlPDF = "$baseURL/PDF_AIPparSSection/$$infos{vaeroproduit}/$$infos{vaeropartie}/$$infos{vaeropartie}-$$infos{vaerosection}.$ad.pdf";
+
+	# https://www.sia.aviation-civile.gouv.fr/dvd/eAIP_19_JUL_2018/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.LFEZ.pdf
+	my $urlPDF = "$baseURL/PDF_AIPparSSection/VAC/AD/AD-2$$infos{vaerosection}.$ad.pdf";
 	#print "$urlPDF\n"; exit;
 	print "$ad.pdf\n";
 	my ($code, $pdf, $cookies) = &sendHttpRequest($urlPDF, SSL_NO_VERIFY => 1, DIE => 0);
@@ -91,32 +77,43 @@ my $pageURL = "$baseURL/FR/VACProduitPartie.htm";  # url de la page de télécharg
   }
 }
 
-
-#    ------------ decode la page html qui permet de choisir un terrain --------------------
-# les infos necessaires se trouvent dans du code javascript, et un bout dans du html :
-#
-# voici un extrait de la page html (ou plutot, du code javascript) 
-#  <script language='JavaScript' type='text/JavaScript'>
-#  <!--
-#  var vaeroportlong =new Array("ABBEVILLE","AGEN LA GARENNE","AIRE SUR L'ADOUR",...
-#  var vaeroproduit ="VAC";
-#  var vaeropartie ="AD";
-#  var vaerosection ="2";
-#  var vaerosoussection =new Array("LFOI","LFBA","LFDA",...
-#  //-->
-#
-# et le bout de html qui contient le prefixe "1706|", 
-#  <input name='Bouton' onClick='clickok("tout","1706_","..|");' type='button'  value='OK' style ="margin-right:21%"/>
-sub decodePage
+#    ------------ recuperation de la date eAIP a partir de la page principale --------------------
+# retourne une chaine du genre "eAIP_19_JUL_2018"
+# ------------------------------------------------------------------------------------------------------------------
+sub getEAIP
 {
   my $page = shift;
   
-  my %infos = ();     # va contenir les infos utiles de la page. En fait, les elements necessaires a la construction de l'url de chaque doc pdf
+  print "## recuperation et traitement de la page d'accueil ##\n";
+  print "   $page\n";
+  my ($code, $page, $cookies) = &sendHttpRequest($siteURL, SSL_NO_VERIFY => 1);
+  die "Impossible de charger la page $siteURL" unless (defined($page));
+
+  my $eAIP;
+  if ( $page =~ /SUP AIP.*\?f=dvd\/(eAIP.*?)\/Atlas\-VAC\/home\.htm\"/)    # "SUP AIP" est la rubrique juste avant  "Atlas VAC FRANCE"
+  {
+    $eAIP = $1
+  }
+  else
+  {
+    die "Pas trouvé eAIP dans la page d'accueil";
+  }
+  return $eAIP;
+}
+
+#    ------------ recuperation ddu code OACI et des noms de terrains a partir d'un script JS --------------------
+# ------------------------------------------------------------------------------------------------------------------
+sub getOACIinfos
+{
+  my $url = shift;
   
-  # on ne teste pas les variables javascripts vaeroproduit, vaeropartie, vaerosection ; on suppose que ca ne change pas.
-  $infos{vaeroproduit} = "VAC";
-  $infos{vaeropartie}  = "AD";
-  $infos{vaerosection} = "2";
+  my $ads = {};
+  
+  print "\n## recuperation des codes OACI des terrains repertories ##\n";
+  print "   $jsURL\n";
+
+  my ($code, $page, $cookies) = &sendHttpRequest($url, SSL_NO_VERIFY => 1);
+  die "Impossible de charger la page $url" unless (defined($page));
   
   die 'pas trouve la variable javascript vaerosoussection (les codes des terrains) dans la page chargee. Voir "page.html"'
     unless ($page =~ /var vaerosoussection =new Array\((.*?)\)/);
@@ -129,18 +126,14 @@ sub decodePage
   my $allNames = $1;   # contient une chaine comme : "ABBEVILLE","AGEN LA GARENNE","AIRE SUR L'ADOUR",...
   $allNames =~ s/"//sg;    # retrait des guillemets
   my @names = split(",", $allNames);
-
-  die 'pas trouve le prefixe (du genre "1706_") dans la page chargee. Voir "page.html"'
-    unless ($page =~ /onClick='clickok\(\"tout\",\"(.*?)_\",\"..\|\"\);'/);
-  $infos{prefixe} = $1;
   
   for (my $ind = 0 ; $ind < scalar(@codes) ; $ind++)
   {
     my $code = $codes[$ind];
-	$infos{ad}{$code}{code} = $code;
-	$infos{ad}{$code}{name} = $names[$ind];
+	$$ads{$code}{code} = $code;
+	$$ads{$code}{name} = $names[$ind];
   }
   
-  return \%infos;
+  return $ads
 }
 
